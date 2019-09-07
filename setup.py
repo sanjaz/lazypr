b78bd218aa75f7c8859a3e9fa9cfb96c3008f3eb
Sanja Segan <sanjazivotic@gmail.com>
2019-09-07 21:49:44 +0200

    Add long description and email in setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
+readme = open('README.md').read()
+
 setup(
     name="lazypr",
-    version="0.1",
+    version="0.1.1",
     description="Creating a pull request on GitHub",
+    long_description=readme,
     author="Sanja Segan",
-    author_email="",
+    author_email="sanjazivotic@gmail.com",
     url="https://github.com/sanjaz/lazypr",
     packages=find_packages(),
     include_package_data=True,
