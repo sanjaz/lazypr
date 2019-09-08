ccee959b9f574387d4e92ca73c01a55120e78288
Sanja Segan <sanjazivotic@gmail.com>
2019-09-08 15:40:09 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lazypr",
-    version="0.1.2",
+    version="0.1.3",
     description="Tool for creating and updating pull requests on GitHub.",
     long_description=long_description,
     long_description_content_type="text/markdown",
